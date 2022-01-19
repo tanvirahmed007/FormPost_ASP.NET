e491403e849bd21a6ddbe9bf8e21dbe813b33a29
tanvi <tanvi@DESKTOP-67ROKOP>
2022-01-19 18:37:06 +0600

    Add project files.

---
+++ FormPost_ASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using FormPost_ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormPost_ASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(PersonalModel personal)
+        {
+            int personId = personal.PersonId;
+            string name = personal.Name;
+            string gender = personal.Gender;
+            string city = personal.City;
+
+            return View();
+
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FormPost_ASP.NET/Models/PersonalModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FormPost_ASP.NET.Models
+{
+    public class PersonalModel
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string City { get; set; }
+    }
+}
